53314b5ca1a797b734bb51fc52e637f3e9dd3b1f
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-09-23 23:11:38 +0530

    Add project files.

---
+++ C# Learn/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+        // Using LINQ to filter even numbers
+        var evenNumbers = from n in numbers
+                          where n % 2 == 0
+                          select n;
+                    
+        Console.WriteLine(numbers);
+        foreach(var n in evenNumbers)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
