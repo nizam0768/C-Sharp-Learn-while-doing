ce1dc85f823bc7544037721ab55df18b788fe603
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-09-25 22:59:51 +0530

    Multiple missing element in array

--- MissingNumber/Program.cs
+++ MissingNumber/Program.cs
@@ -1,5 +1,19 @@
 ﻿class Program
 {
+    //If multiple numbers are missing
+    public List<int> FindMissingElements(int[] arr, int n)
+    {
+        var present = new HashSet<int>(arr);
+        var missing = new List<int>();
+        for(int i = 1; i<=n; i++)
+        {
+            if(!present.Contains(i))
+            {
+                missing.Add(i);
+            }
+        }
+        return missing;
+    }
     public int MissingNumber(int[] array, int n)
     {
         // Get the some n natural number 
@@ -17,6 +31,15 @@
         int[] arr = { 1, 2, 3, 5};
         Program p = new Program();
         int missingNum = p.MissingNumber(arr, 5);
+        Console.WriteLine("---------------Single Element Missing-----------------");
         Console.WriteLine(missingNum);
+
+        int[] arr2 = { 1, 2, 4, 6, 7, 9 };
+        List<int> multipleMissing = p.FindMissingElements(arr2, 8);
+        Console.WriteLine("--------------------Multiple Element Missing-----------------");
+        foreach(int num in multipleMissing)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
