fb4c72e6cc820939e4a6d401dc4d2c8047521473
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-09-26 00:17:17 +0530

    Practicing

---
+++ ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "My Name is Khan";
+        string reversed = "";
+        for (int i = input.Length-1; i>=0; i--)
+        {
+            reversed += input[i];
+        }
+        Console.WriteLine("Orignam String: " + input);
+        Console.WriteLine("Reversed String:" + reversed);
+    }
+    
+}
