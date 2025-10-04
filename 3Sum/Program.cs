c226874e0aded6e54e0774dec34562a927d9252e
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-10-04 22:06:02 +0530

    find 3Sum

---
+++ 3Sum/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        Array.Sort(nums); // Sort the array
+        List<IList<int>> result = new List<IList<int>>();
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0)
+                {
+                    result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] num = { -1, 0, 1, 2, -1, -4 };
+        Program program = new Program();
+        program.ThreeSum(num);
+    }
+}
