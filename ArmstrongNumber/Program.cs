6d6b97dc46b89db2db7cb061822cd0327eab0319
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-10-05 19:38:54 +0530

    armstrong number

---
+++ ArmstrongNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the starting number: ");
+        int start = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the ending number: ");
+        int end = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nArmstrong numbers between {start} and {end} are:");
+
+        for (int i = start; i <= end; i++)
+        {
+            if (IsArmstrong(i))
+                Console.WriteLine(i);
+        }
+    }
+
+    static bool IsArmstrong(int num)
+    {
+        int originalNum = num;
+        int sum = 0;
+        int digits = num.ToString().Length;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            sum += (int)Math.Pow(digit, digits);
+            num /= 10;
+        }
+
+        return sum == originalNum;
+    }
+}
