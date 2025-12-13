80d363aae8420ff3bb8a710270d1859f062ed1be
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-12-13 22:23:35 +0530

    Using record in c sharpp

---
+++ RecordInCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿record City(string Name, long Population);
+class Program
+{
+    static readonly City[] cities = [
+        new City("Tokyo", 37_833_000),
+        new City("Delhi", 30_290_000),
+        new City("Shanghai", 27_110_000),
+        new City("São Paulo", 22_043_000),
+        new City("Mumbai", 20_412_000),
+        new City("Beijing", 20_384_000),
+        new City("Cairo", 18_772_000),
+        new City("Dhaka", 17_598_000),
+        new City("Osaka", 19_281_000),
+        new City("New York-Newark", 18_604_000),
+        new City("Karachi", 16_094_000),
+        new City("Chongqing", 15_872_000),
+        new City("Istanbul", 15_029_000),
+        new City("Buenos Aires", 15_024_000),
+        new City("Kolkata", 14_850_000),
+        new City("Lagos", 14_368_000),
+        new City("Kinshasa", 14_342_000),
+        new City("Manila", 13_923_000),
+        new City("Rio de Janeiro", 13_374_000),
+        new City("Tianjin", 13_215_000)];
+    static void Main(string[] args)
+    {
+        IEnumerable<City> queryMajorCities =
+            from city in cities
+            where city.Population > 30_000_000
+            select city;
+
+        foreach (City city in queryMajorCities)
+        {
+            Console.WriteLine($"{city.Name} : {city.Population}");
+        }
+    }
+}
