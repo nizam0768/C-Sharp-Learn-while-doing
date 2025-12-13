8399736199098b44ce1ab605f4374c0213ae8964
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-12-13 20:57:22 +0530

    C# Practice

---
+++ SecondLargestNum/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        int[] numbers = { 10, 35, 20, 50, 40 };
+        int largest = int.MinValue;
+        int secondLargest = int.MinValue;
+        foreach (int num in numbers) { 
+            if(num > largest)
+            {
+                secondLargest = largest;
+                largest = num;
+            }
+            else if(num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+        Console.WriteLine("Second Largest {0}", secondLargest);
+    }
+}
